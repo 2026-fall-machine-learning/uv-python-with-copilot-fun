4ca066d5f0ad179d0e0aa7f351c8b49a04533e15
tsteelematc <tsteele@madisoncollege.edu>
2026-09-14 10:08:10 -0500

    prepping week 03 mon

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pyfiglet
 
 def main() -> None:
     print(pyfiglet.figlet_format("Hello world ! ! !"))
+    print(pyfiglet.figlet_format("do_s"))
+    print(pyfiglet.figlet_format("u_"))
+    print(pyfiglet.figlet_format("_eally"))
+    print(pyfiglet.figlet_format("r_ally"))
+    print(pyfiglet.figlet_format("real_y"))
+    print(pyfiglet.figlet_format("r_ck"))
+    print(pyfiglet.figlet_format("!? !_ !?"))
+
 
 
 if __name__ == "__main__":
