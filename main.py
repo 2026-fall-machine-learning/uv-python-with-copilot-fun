1e3100a96ae01848e604708ed5317b0c5799a43a
narudser <nrudser@madisoncollege.edu>
2026-09-16 15:05:11 -0500

    fixed the u bug

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyfiglet
 def main() -> None:
     print(pyfiglet.figlet_format("Hello world ! ! !"))
     print(pyfiglet.figlet_format("do_s"))
-    print(pyfiglet.figlet_format("u_"))
+    print(pyfiglet.figlet_format("uv"))
     print(pyfiglet.figlet_format("_eally"))
     print(pyfiglet.figlet_format("r_ally"))
     print(pyfiglet.figlet_format("real_y"))
